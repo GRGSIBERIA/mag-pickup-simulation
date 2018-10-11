1df8d5c53f716e74e081267cff66e2fa0ffc6929
grgsiberia <nanashi4129@gmail.com>
2018-10-11 19:18:56 +0900

    行列に直して計算が通るように工夫した結果，処理が戻らない

--- sym/simulation.py
+++ sym/simulation.py
@@ -3,11 +3,15 @@
 Author: Eiichi Takebuchi(GRGSIBERIA)
 """
 import sympy as sym
-from sympy.vector import CoordSys3D
+import numpy as np
 
-p = CoordSys3D("p")
-pd = CoordSys3D("p'")
-P = CoordSys3D("P")
+px, py, pz = sym.symbols("p_x p_y p_z")
+pdx, pdy, pdz = sym.symbols("p'_x p'_y p'_z")
+Px, Py, Pz = sym.symbols("P_x P_y P_z")
+
+p = sym.Matrix([[px, py, pz]])
+pd = sym.Matrix([[pdx, pdy, pdz]])
+P = sym.Matrix([[Px, Py, Pz]])
 
 sigma = sym.Symbol("\\sigma")
 rho = sym.Symbol("\\rho")
@@ -16,14 +20,14 @@ Phi = sym.Symbol("\\Phi")
 theta = sym.Symbol("\\theta")
 gamma = sym.Symbol("\\gamma")
 
-xi = CoordSys3D("\\hat\{x\}")
-zi = CoordSys3D("\\hat\{z\}")
+xi = sym.Matrix([[1, 0, 0]])
 
 phi = sym.Matrix((
     [sym.cos(theta), -sym.sin(theta), 0], 
     [sym.sin(theta), sym.cos(theta), 0],
     [0, 0, 1]))
 
-frac_under = type(rho * xi * phi)
-print(frac_under)
+func_under = (rho * xi * phi + pd + (-p)).norm()**2.
+Bz = (sigma * (pdz - pz) * rho) / func_under
 
+sym.integrate(sym.integrate(Bz, (rho, 0, psi)), (theta, 0, 2. * np.pi))
