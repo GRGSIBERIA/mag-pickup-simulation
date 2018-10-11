387b36e50c37442417afd70b73e4826bf98aa881
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-10-12 01:32:53 +0900

    細かい計算を作る

--- np/simulation.py
+++ np/simulation.py
@@ -7,7 +7,7 @@ from scipy import integrate
 
 class FieldStrengthOfMagneticMonopole:
     """
-    単極磁石の磁束密度を求める
+    単極磁石の電界の強さを求める
     """
     def __init__(self, origin: np.array, target: np.array, sigma: float, Psi: float):
         """
@@ -30,31 +30,60 @@ class FieldStrengthOfMagneticMonopole:
             [np.cos(theta), -np.sin(theta), 0],
             [np.sin(theta), np.cos(theta), 0],
             [0, 0, 1]))
+    
+    def _frac_under_out(self, _d_rho, _d_theta):
+        frac_under_in = _d_rho * self.xi * self._phi(_d_theta) + self.target - self.origin
+        return np.power(np.linalg.norm(frac_under_in), 2.)
 
     def _compute_Bz_in(self, _d_rho: float, _d_theta: float):
         """
-        磁束密度の内側の式
+        電界の強さの内側の式
         """
-        frac_under_in = _d_rho * self.xi * self._phi(_d_theta) + self.target - self.origin
-        frac_under_out = np.power(np.linalg.norm(frac_under_in), 2.)
+        frac_under_out = self._frac_under_out(_d_rho, _d_theta)
         frac_upper = self.sigma * (self.target[2] - self.origin[2]) * _d_rho
+        return frac_upper / frac_under_out
+
+    def _compute_Bw_in(self, _d_rho: float, _d_theta: float):
+        """
+        ワイヤ上の電界の強さの内側の式
+        """
+        frac_under_out = self._frac_under_out(_d_rho, _d_theta)
+        frac_upper = self.sigma * _d_rho
         return frac_upper / frac_under_out
 
     def computeBz(self):
         """
-        原点から対象位置にかけて原点X-Y平面の磁束密度を求める
+        原点から対象位置にかけて原点X-Y平面の電界の強さを求める
+        :return: [0]は値, [1]は推定誤差
+        """
+        return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
+    
+    def computeBw(self):
+        """
+        原点から対象位置にかけて原点X-Y平面の電界の強さを求める
         :return: [0]は値, [1]は推定誤差
         """
         return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
 
+    def computeBwz(self, _P: np.array, _gamma: float):
+        """
+        :param _P: ワイヤから誘導された磁場
+        :param _gamma: ワイヤの局所磁場の比例定数
+        :return: ワイヤから見たときの電界の強さ
+        """
+        upper = _gamma * self.computeBw()[0] * (self.target[2] - _P[2])
+        under = np.power(np.linalg.norm(self.target - _P), 2.)
+        return upper / under
 
 if __name__ == "__main__":
     p = np.array([0, 0, 0])
     pd = np.array([2, 2, 2])
-
-    sigma = 1
-    Psi = 1
+    P = np.array([3, 3, 3])
+    
+    sigma = 5
+    Psi = 3
+    gamma = 1
 
     fsmm = FieldStrengthOfMagneticMonopole(p, pd, sigma, Psi)
 
-    print(fsmm.computeBz())
+    print(fsmm.computeBwz(P, gamma))
