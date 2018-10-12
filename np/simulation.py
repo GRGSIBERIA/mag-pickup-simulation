8838a90ba2f0e85122ef666f86a48ed9cb04f31d
grgsiberia <nanashi4129@gmail.com>
2018-10-12 15:45:50 +0900

    コメントを追加

--- np/simulation.py
+++ np/simulation.py
@@ -7,17 +7,17 @@ from scipy import integrate
 
 class FieldStrengthOfMagneticMonopole:
     """
-    単極磁石の電界の強さを求める
+    磁気単極子の磁束密度を求める
     """
-    def __init__(self, origin: np.array, target: np.array, sigma: float, Psi: float):
+    def __init__(self, origin: np.array, target: np.array, _sigma: float, _Psi: float):
         """
         :param origin: 原点
-        :param target: 測定点
-        :param sigma: 電荷密度
-        :param Psi: 測定半径
+        :param target: 測定点（ワイヤー位置）
+        :param _sigma: 電荷密度
+        :param _Psi: 測定半径
         """
-        self.sigma = sigma 
-        self.Psi = Psi
+        self.sigma = _sigma 
+        self.Psi = _Psi
         self.xi = np.array([1, 0, 0])
         self.origin = origin 
         self.target = target
@@ -37,7 +37,7 @@ class FieldStrengthOfMagneticMonopole:
 
     def _compute_Bz_in(self, _d_rho: float, _d_theta: float):
         """
-        電界の強さの内側の式
+        磁束密度の内側の式
         """
         frac_under_out = self._frac_under_out(_d_rho, _d_theta)
         frac_upper = self.sigma * (self.target[2] - self.origin[2]) * _d_rho
@@ -45,7 +45,7 @@ class FieldStrengthOfMagneticMonopole:
 
     def _compute_Bw_in(self, _d_rho: float, _d_theta: float):
         """
-        ワイヤ上の電界の強さの内側の式
+        ワイヤ上の磁束密度の内側の式
         """
         frac_under_out = self._frac_under_out(_d_rho, _d_theta)
         frac_upper = self.sigma * _d_rho
@@ -53,23 +53,23 @@ class FieldStrengthOfMagneticMonopole:
 
     def computeBz(self):
         """
-        原点から対象位置にかけて原点X-Y平面の電界の強さを求める
+        原点から対象位置にかけて原点X-Y平面の磁束密度を求める
         :return: [0]は値, [1]は推定誤差
         """
         return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
     
     def computeBw(self):
         """
-        原点から対象位置にかけて原点X-Y平面の電界の強さを求める
+        原点から対象位置にかけて原点X-Y平面の磁束密度を求める
         :return: [0]は値, [1]は推定誤差
         """
         return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
 
     def computeBwz(self, _P: np.array, _gamma: float):
         """
-        :param _P: ワイヤから誘導された磁場
+        :param _P: ワイヤから誘導された磁場の測定点
         :param _gamma: ワイヤの局所磁場の比例定数
-        :return: ワイヤから見たときの電界の強さ
+        :return: ワイヤから見たときの磁束密度
         """
         upper = _gamma * self.computeBw()[0] * (self.target[2] - _P[2])
         under = np.power(np.linalg.norm(self.target - _P), 2.)
@@ -80,9 +80,9 @@ if __name__ == "__main__":
     pd = np.array([2, 2, 2])
     P = np.array([3, 3, 3])
     
-    sigma = 5
-    Psi = 3
-    gamma = 1
+    sigma = 5   # 電荷密度
+    Psi = 3     # 磁気単極子の半径
+    gamma = 1   # 永久磁石で誘導されたワイヤの局所磁場
 
     fsmm = FieldStrengthOfMagneticMonopole(p, pd, sigma, Psi)
 
