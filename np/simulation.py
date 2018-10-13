e2b5902b4b346cca103c76e28a4555049cc8cc87
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-10-14 01:51:56 +0900

    コメント追記

--- np/simulation.py
+++ np/simulation.py
@@ -8,11 +8,15 @@ from scipy import integrate
 class FieldStrengthOfMagneticMonopole:
     """
     磁気単極子の磁束密度を求める
+
+    .. Note:
+    このクラスは磁気単極子がZ軸を磁極として，常にX-Y平面を取ると仮定する
+    従って，磁気単極子の向きがズレた場合，ワイヤー位置もズレるのでデータを渡すときに変換すること
     """
-    def __init__(self, origin: np.array, target: np.array, _sigma: float, _Psi: float):
+    def __init__(self, origin: np.array, wire_point: np.array, _sigma: float, _Psi: float):
         """
-        :param origin: 原点
-        :param target: 測定点（ワイヤー位置）
+        :param origin: 磁気単極子の原点
+        :param wire_point: ワイヤー位置
         :param _sigma: 電荷密度
         :param _Psi: 測定半径
         """
@@ -20,7 +24,7 @@ class FieldStrengthOfMagneticMonopole:
         self.Psi = _Psi
         self.xi = np.array([1, 0, 0])
         self.origin = origin 
-        self.target = target
+        self.wire_point = wire_point
 
     def _phi(self, theta: float) -> np.matrix:
         """
@@ -32,7 +36,7 @@ class FieldStrengthOfMagneticMonopole:
             [0, 0, 1]))
     
     def _frac_under_out(self, _d_rho, _d_theta):
-        frac_under_in = _d_rho * self.xi * self._phi(_d_theta) + self.target - self.origin
+        frac_under_in = _d_rho * self.xi * self._phi(_d_theta) + self.wire_point - self.origin
         return np.power(np.linalg.norm(frac_under_in), 2.)
 
     def _compute_Bz_in(self, _d_rho: float, _d_theta: float):
@@ -40,7 +44,7 @@ class FieldStrengthOfMagneticMonopole:
         磁束密度の内側の式
         """
         frac_under_out = self._frac_under_out(_d_rho, _d_theta)
-        frac_upper = self.sigma * (self.target[2] - self.origin[2]) * _d_rho
+        frac_upper = self.sigma * (self.wire_point[2] - self.origin[2]) * _d_rho
         return frac_upper / frac_under_out
 
     def _compute_Bw_in(self, _d_rho: float, _d_theta: float):
@@ -53,26 +57,26 @@ class FieldStrengthOfMagneticMonopole:
 
     def computeBz(self):
         """
-        原点から対象位置にかけて原点X-Y平面の磁束密度を求める
+        原点からワイヤ位置にかけて原点X-Y平面の磁束密度を求める
         :return: [0]は値, [1]は推定誤差
         """
         return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
     
     def computeBw(self):
         """
-        原点から対象位置にかけて原点X-Y平面の磁束密度を求める
+        原点からワイヤ位置にかけて原点X-Y平面の磁束密度を求める
         :return: [0]は値, [1]は推定誤差
         """
         return integrate.nquad(self._compute_Bz_in, [[0, self.Psi], [0, 2. * np.pi]])
 
-    def computeBwz(self, _P: np.array, _gamma: float):
+    def computeBwz(self, _MP: np.array, _gamma: float):
         """
-        :param _P: ワイヤから誘導された磁場の測定点
+        :param _MP: 測定点(Measurement Point)
         :param _gamma: ワイヤの局所磁場の比例定数
         :return: ワイヤから見たときの磁束密度
         """
-        upper = _gamma * self.computeBw()[0] * (self.target[2] - _P[2])
-        under = np.power(np.linalg.norm(self.target - _P), 2.)
+        upper = _gamma * self.computeBw()[0] * (self.wire_point[2] - _MP[2])
+        under = np.power(np.linalg.norm(self.wire_point - _MP), 2.)
         return upper / under
 
 if __name__ == "__main__":
