3316dc6f32bda4bf1293effbfd25281f2feb2142
grgsiberia <nanashi4129@gmail.com>
2018-10-24 16:13:16 +0900

    半径を大きくさせたときの磁束密度

---
+++ nump/change_radius.py
@@ -0,0 +1,19 @@
+#-*- encoding: utf-8
+
+import matplotlib.pyplot as plot
+import numpy as np
+from system.simulation import FieldStrengthOfMagneticMonopole
+
+bws = []
+count = 100
+for radius in range(count):
+    fsmm = FieldStrengthOfMagneticMonopole(np.array([0, 0, 0]), np.array([0, 0, 5]), 1, radius)
+    bws.append(fsmm.computeBw()[0])
+bws = np.array(bws)
+
+rads = np.linspace(0, count, count)
+
+plot.plot(rads, bws)
+plot.xlabel("-> Radius [mm]")
+plot.ylabel("-> Relative magnetic flux density [G]")
+plot.show()
