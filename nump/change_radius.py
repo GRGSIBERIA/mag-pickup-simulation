042465a1c9fce93635131ea849b4d8bb1a718dcb
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-10-25 21:12:31 +0900

    位置を5～25mmで5mm区切りで出力

--- nump/change_radius.py
+++ nump/change_radius.py
@@ -4,16 +4,25 @@ import matplotlib.pyplot as plot
 import numpy as np
 from system.simulation import FieldStrengthOfMagneticMonopole
 
-bws = []
-count = 1000
-for radius in range(count):
-    fsmm = FieldStrengthOfMagneticMonopole(np.array([0, 0, 0]), np.array([0, 0, 5]), 1, radius)
-    bws.append(fsmm.computeBz()[0])
-bws = np.array(bws)
+def plotBz(zlength):
+    bws = []
+    count = 100
+    for radius in range(count):
+        fsmm = FieldStrengthOfMagneticMonopole(np.array([0, 0, 0]), np.array([0, 0, zlength]), 1, radius)
+        bws.append(fsmm.computeBz()[0])
+    bws = np.array(bws)
 
-rads = np.linspace(0, count, count)
+    rads = np.linspace(0, count, count)
 
-plot.plot(rads, bws)
+    plot.plot(rads, bws, label="Displacement of z-axis on {0} mm".format(zlength))
+
+plotBz(5)
+plotBz(10)
+plotBz(15)
+plotBz(20)
+plotBz(25)
+
 plot.xlabel("-> Radius [mm]")
 plot.ylabel("-> Relative magnetic flux density [G]")
+plot.legend()
 plot.show()
